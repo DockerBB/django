c9044c0217cc3b170898c73ac970fcd146d9cbac
DockerBB <30626509+DockerBB@users.noreply.github.com>
2019-08-23 15:56:08 +0800

    显示分块

--- HelloWorld/TestModel/admin.py
+++ HelloWorld/TestModel/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from TestModel.models import Test,Contact,Tag
+from TestModel.models import Test, Contact, Tag
+
 
 # Register your models here.
 class ContactAdmin(admin.ModelAdmin):
-    fields = ('name', 'email')
+    fieldsets = (
+        ['Main', {
+            'fields': ('name', 'email'),
+        }],
+        ['Advance', {
+            'classes': ('collapse',),  # CSS
+            'fields': ('age',),
+        }]
+    )
 
 
 admin.site.register(Contact, ContactAdmin)
